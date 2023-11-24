e7fd90dbcdc8b4ba22caff822f657a2b7ea1733c
Lucas <41449194+lucasph123@users.noreply.github.com>
2023-11-24 17:24:50 -0300

    added winning condition

--- script.js
+++ script.js
@@ -4,16 +4,48 @@ function startGame()
     const cpu = cpuPlay();
     const player = playerPlay();
 
-   // do{
-    game.gameMap[game.gameMap.indexOf(player.choice())] = 'X';
-    console.log(game.gameMap);
+    do{
+    // game.gameMap[game.gameMap.indexOf(player.choice())] = 'X';
+    // console.log(player.playerPlayed);
     game.gameMap[game.gameMap.indexOf(cpu.choice())] = 'O';
+    console.log(cpu.cpuPlayed);
     console.log(game.gameMap);
-    console.log(player.playerPlayed);
-    //}while(winCondition() != false)
+
+    }while(winCondition(player.playerPlayed,cpu.cpuPlayed) == false)
 
     
 }
+function winCondition(player, cpu)
+{
+    if(multiplesInArray(player,[1,2,3])  || multiplesInArray(player,[4,5,6])||
+        multiplesInArray(player,[7,8,9]) || multiplesInArray(player,[1,4,7]) ||
+        multiplesInArray(player,[2,5,8]) || multiplesInArray(player,[3,6,9]) ||
+        multiplesInArray(player,[1,5,9]) || multiplesInArray(player,[3,5,7]) )
+        {
+            console.log('ture');
+            return true;
+    }else if (multiplesInArray(cpu,[1,2,3])  || multiplesInArray(cpu,[4,5,6])||
+            multiplesInArray(cpu,[7,8,9]) || multiplesInArray(cpu,[1,4,7]) ||
+            multiplesInArray(cpu,[2,5,8]) || multiplesInArray(cpu,[3,6,9]) ||
+            multiplesInArray(cpu,[1,5,9]) || multiplesInArray(cpu,[3,5,7]) )
+        {
+            console.log('ture cpu');
+        }
+        else
+        {
+            console.log('falsee');
+            return false;
+        }
+       
+    /*
+    Win Conditions
+    [X X X]    [X * *]    [* * X]   [* X *]   [* * *]   [* * *]   [X * *]  [* * X]  [1 2 3]     
+    [* * *]    [X * *]    [* * X]   [* X *]   [X X X]   [* * *]   [* X *]  [* X *]  [4 5 6] 
+    [* * *]    [X * *]    [* * X]   [* X *]   [* * *]   [X X X]   [* * X]  [X * *]  [7 8 9] 
+    
+    */
+}
+
 
 function gameMap()
 {
@@ -53,7 +85,12 @@ function playerPlay()
     return {playerPlayed,choice};
 
 }
+function multiplesInArray(array,values)
+{
+    return values.every(value => {
+        return array.includes(value);
+     })
+
+}
 
 startGame();
-
-    
